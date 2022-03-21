7a8c9e3998f499f8c5fff74db508f8afc7fb0c5e
celdom2020 <7327737@gmail.com>
2022-03-21 19:57:29 +0300

    2lesson profile

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,11 @@
 import React from "react";
-import './navbar.css';
+import './Navbar.css';
 function Navbar() {
     return (
         <div className="navbar">
-            <h1>navbar</h1>
-            <a className="navbarlink" href="#">BLOG</a>
-            <a className="navbarlink" href="#">CONTACT</a>
-            <a className="navbarlink" href="#">PRICE</a>
+            <a href="#">Profile</a>
+            <a href="#">Messages</a>
+            <a href="#">Users</a>
         </div>
     )
 }
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,8 +1,23 @@
 import React from "react";
+import './Profile.css'
+import elon from '../../img/elon.jpg';
+import elonmini from '../../img/elon_mini.jpg';
 function Profile() {
     return (
         <div className="profile">
-            <h1>profile</h1>
+            <div className="me">
+                <img src={elon} alt="Здесь"/>
+                <p>Elon Musk</p>
+            </div>
+            <div className="posts">
+                <input placeholder="Введите сообщение"/>
+                <button>Добавить сообщение</button>
+                <div className="post">
+                    <img src={elonmini}/>
+                    <span>Elon Musk</span>
+                    <p>Some text</p>
+                </div>
+            </div>
         </div>
     )
 }
