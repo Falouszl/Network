8f14c6b6f35caeba29a0822e69f8308a092de837
celdom2020 <7327737@gmail.com>
2022-03-21 19:03:16 +0300

    Navbar+Links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,12 @@
 import React from "react";
+import './navbar.css';
 function Navbar() {
     return (
         <div className="navbar">
             <h1>navbar</h1>
+            <a className="navbarlink" href="#">BLOG</a>
+            <a className="navbarlink" href="#">CONTACT</a>
+            <a className="navbarlink" href="#">PRICE</a>
         </div>
     )
 }
